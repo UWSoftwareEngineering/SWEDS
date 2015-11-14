f9878561a44138f5bdef09c81a8b77e7d87326f9
Joseph Hellerstein <jlheller@uw.edu>
2015-11-14 12:04:04 -0800

    Beginning refactor of accessing data.

--- Code/pronto_data.py
+++ Code/pronto_data.py
@@ -1,30 +1,34 @@
 '''Reads the data into a pandas dataframe.'''
 
-/*
+'''
 BUGS
-1. file ends up in Code, not Data
-2. Setting paths in ipython
-*/
+1. Read trips file, not general zip
+'''
 
 import os
+import shutil
 import pandas as pd
 import numpy as np
 from subprocess import call
 
-def ProntoData(data_set="open_data_year_one.zip",
+def ProntoData(file_name="open_data_year_one",
     url="https://s3.amazonaws.com/pronto-data/open_data_year_one.zip"
     ):
   # Returns a Pandas data frame
   parent_path = os.path.abspath("..")
-  abs_path = os.path.join(parent_path, "Data/%s" % data_set)
+  zip_file = "%s.zip" % file_name
+  csv_file = "%s.csv" % file_name
+  zip_abs_path = os.path.join(parent_path, "Data/%s" % zip_file)
+  csv_abs_path = os.path.join(parent_path, "Data/%s" % csv_file)
   import pdb; pdb.set_trace()
-  if not os.path.exists(abs_path):
-    call(["curl", 
-        "-O", 
-        url
-        ])
-    call(["unzip", abs_path])
-  result = pd.read_csv(abs_path,
+  if not os.path.exists(zip_abs_path):
+#    call(["curl", 
+#        "-O", 
+#        url
+#        ])
+    shutil.move(zip_file, zip_abs_path)
+    call(["unzip", zip_abs_path])
+  result = pd.read_csv(csv_abs_path,
       parse_dates=['starttime', 'stoptime'],
       infer_datetime_format=True)
   return result
