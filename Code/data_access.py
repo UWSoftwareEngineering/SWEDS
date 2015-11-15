1e4a050493d9a707f839bea8cf36b1aad8d3392c
Joseph Hellerstein <jlheller@uw.edu>
2015-11-15 08:41:55 -0800

    Refined and refactored data_access

--- Code/data_access.py
+++ Code/data_access.py
@@ -3,9 +3,8 @@
    data if it is not present.
 
 BUGS/FEATURES
-1. Tests
-2. Separate access to different data files
-3. Deal with unzip file exists
+1. Test getTripData
+2. Deal with unzip file exists
 '''
 
 import os
@@ -14,30 +13,60 @@ import pandas as pd
 import numpy as np
 from subprocess import call
 
-def prontoData(file_name="open_data_year_one",
+PRONTO_FILES = ["2015_station_data.csv", "2015_status_data.csv",
+                "2015_trip_data.csv", "2015_weather_data.csv",
+                "README.txt", "open_data_year_one.zip"]
+PARENT_PATH = os.path.abspath("..")
+DATA_DIR = os.path.join(PARENT_PATH, "Data")
+
+class StackDir(object):
+
+  '''This class provides a stack to easily navigate directories.'''
+
+  def __init__(self):
+    self.stack = []
+
+  def push(self, new_directory):
+    # Move to the new directory, remembering the current directory
+    # Input: new_directory - new directory
+    self.stack.insert(0, os.getcwd())
+    os.chdir(new_directory)
+
+  def pop(self):
+    # Move to the previous directory
+    new_directory = self.stack.pop()
+    os.chdir(new_directory)
+
+
+def getProntoFiles(file_name="open_data_year_one",
     url="https://s3.amazonaws.com/pronto-data/open_data_year_one.zip"
     ):
   # Makes sure that the data are downloaded into Data/
-  # Changes to the data directory
-  parent_path = os.path.abspath("..")
-  data_dir = os.path.join(parent_path, "Data")
-  os.chdir(data_dir)
+  stack_dir = StackDir()
+  stack_dir.push(DATA_DIR)
   zip_file = "%s.zip" % file_name
   if not os.path.exists(zip_file):
-    os.chdir
     call(["curl", 
         "-O", 
         url
         ])
-    import pdb; pdb.set_trace()
     call(["unzip", zip_file])
+  stack_dir.pop()
   return
 
-def tripData():
+def deleteDataFiles(file_list = PRONTO_FILES):
+  # Deletes files from the data directory
+  stack_dir = StackDir()
+  stack_dir.push(DATA_DIR)
+  for item in file_list:
+    os.remove(item)
+  stack_dir.pop()
+
+def getTripData():
   # Returns a pandas dataframe with the trip data
-  prontoData()
+  getProntoFiles()
   TRIP_FILE = "2015_trip_data.csv"
-  abs_path = os.path.join(parent_path, "Data/%s" % TRIP_FILE)
+  abs_path = os.path.join(PARENT_PATH, "Data/%s" % TRIP_FILE)
   result = pd.read_csv(abs_path,
       parse_dates=['starttime', 'stoptime'],
       infer_datetime_format=True)
