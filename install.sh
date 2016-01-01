c5f4e5f7827ee438a25ef0d798c92431a5c4f7f5
Joseph Hellerstein <jlheller@uw.edu>
2016-01-01 10:49:38 -0800

    Verified install scripts

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ echo "Install atom"
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
+echo "Creating data directory..."
+mkdir Data
 echo "Install miniconda"
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >miniconda_setup.sh
 bash miniconda_setup.sh
